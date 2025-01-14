dc5993791391a4c1ef07e6c59c3930b72c321d77
jmbouzan <jose.manuel@bouzan.net>
2025-01-14 13:30:55 +0100

    Add files via upload

--- lang/ro/ardora.php
+++ lang/ro/ardora.php
@@ -18,12 +18,24 @@
  * Strings for component 'ardora', language 'ro'
  *
  * @package    mod_ardora
- * @copyright  2024 José Manuel Bouzán Matanza
+ * @copyright  2025 José Manuel Bouzán Matanza
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['ardora:addinstance'] = 'Adăugați un nou ardora';
+$string['ardora:exportardora'] = 'Exportați ardora';
+$string['ardora:grade'] = 'Evaluează livrările Ardora';
+$string['ardora:view'] = 'Vizualizați ardora';
+$string['ardoracontent'] = 'Fișiere și subfoldere';
+$string['ardoradetails_sizedate'] = '{$a->size} {$a->date}';
+$string['ardoradetails_sizetype'] = '{$a->size} {$a->type}';
+$string['ardoradetails_sizetypedate'] = '{$a->size} {$a->type} {$a->date}';
+$string['ardoradetails_typedate'] = '{$a->type} {$a->date}';
+$string['cachedef_courseid_cache'] = 'Cache pentru stocarea ID-urilor cursurilor în sesiune.';
 $string['clicktodownload'] = 'Faceți clic pe linkul {$a} pentru a descărca fișierul.';
 $string['clicktoopen2'] = 'Faceți clic pe linkul {$a} pentru a vizualiza fișierul.';
+$string['completionpassgrade'] = 'Studentul trebuie să obțină nota de trecere pentru a finaliza această activitate.';
+$string['completionpassgrade_help'] = 'Dacă această opțiune este activată, activitatea va fi marcată ca finalizată doar atunci când studentul obține nota de trecere specificată.';
 $string['configdisplayoptions'] = 'Selectați toate opțiunile care ar trebui să fie disponibile, setările existente nu vor fi modificate. Țineți apăsată tasta CTRL pentru a selecta mai multe câmpuri.';
 $string['configframesize'] = 'Când o pagină web sau un fișier încărcat este afișat într-un cadru, această valoare este înălțimea (în pixeli) a cadrului superior (care conține navigarea).';
 $string['configparametersettings'] = 'Aceasta setează valoarea implicită pentru panoul de setări Parametri din formular atunci când adăugați noi ardoras. După prima utilizare, aceasta devine o preferință individuală a utilizatorului.';
@@ -38,6 +50,7 @@ $string['configpopupstatus'] = 'Ferestrele pop-up ar trebui să afișeze bara de
 $string['configpopuptoolbar'] = 'Ferestrele pop-up ar trebui să afișeze bara de instrumente în mod implicit?';
 $string['configpopupwidth'] = 'Care ar trebui să fie lățimea implicită pentru noile ferestre pop-up?';
 $string['contentheader'] = 'Conținut';
+$string['courseidnotfound'] = 'ID-ul cursului nu a fost găsit.';
 $string['displayoptions'] = 'Opțiuni disponibile de afișare';
 $string['displayselect'] = 'Afișare';
 $string['displayselect_help'] = 'Această setare, împreună cu tipul de fișier și dacă browserul permite încorporarea, determină modul în care este afișat fișierul. Opțiunile pot include:
@@ -52,6 +65,10 @@ $string['displayselect_help'] = 'Această setare, împreună cu tipul de fișier
 $string['displayselect_link'] = 'mod/file/mod';
 $string['displayselectexplain'] = 'Alegeți tipul de afișare, din păcate, nu toate tipurile sunt potrivite pentru toate fișierele.';
 $string['dnduploadardora'] = 'Creați un fișier ardora';
+$string['embedheight'] = 'Înălțime';
+$string['embedheightexplain'] = 'Înălțimea cadrului scenei.';
+$string['embedwidth'] = 'Lățime';
+$string['embedwidthexplain'] = 'Lățimea cadrului scenei.';
 $string['encryptedcode'] = 'Cod criptat';
 $string['filenotfound'] = 'Fișierul nu a fost găsit, ne pare rău.';
 $string['filterfiles'] = 'Folosiți filtre pe conținutul fișierului';
@@ -59,6 +76,7 @@ $string['filterfilesexplain'] = 'Selectați tipul de filtrare a conținutului fi
 $string['filtername'] = 'Linkare automată a numelor ardora';
 $string['forcedownload'] = 'Forțează descărcarea';
 $string['framesize'] = 'Înălțimea cadrului';
+$string['gradingoptions'] = 'Opțiuni de notare';
 $string['indicator:cognitivedepth'] = 'Adâncime cognitivă a fișierului';
 $string['indicator:cognitivedepth_help'] = 'Acest indicator se bazează pe adâncimea cognitivă atinsă de student într-un ardora de fișier.';
 $string['indicator:cognitivedepthdef'] = 'Adâncime cognitivă a fișierului';
@@ -69,9 +87,12 @@ $string['indicator:socialbreadth_help'] = 'Acest indicator se bazează pe lăți
 $string['indicator:socialbreadthdef'] = 'Lățime socială a fișierului';
 $string['indicator:socialbreadthdef_help'] = 'Participantul a atins acest procentaj de angajament social oferit de ardoras de fișier în timpul acestui interval de analiză (Niveluri = Fără participare, Participant singur)';
 $string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
+$string['invalidpassinggrade'] = 'Nota de trecere trebuie să fie un număr între 0 și 100.';
 $string['legacyfiles'] = 'Migrarea fișierelor vechi de curs';
 $string['legacyfilesactive'] = 'Activ';
 $string['legacyfilesdone'] = 'Finalizat';
+$string['maximumgrade'] = 'Nota maximă';
+$string['maximumgrade_help'] = 'Specificați nota maximă care poate fi obținută pentru această activitate.';
 $string['modifieddate'] = 'Modificat {$a}';
 $string['modulename'] = 'Ardora';
 $string['modulename_help'] = 'Ajutor explicativ aici';
@@ -80,37 +101,21 @@ $string['modulenameplural'] = 'Fișiere';
 $string['notmigrated'] = 'Acest tip de ardora moștenit ({$a}) nu a fost încă migrat, ne pare rău.';
 $string['optionsheader'] = 'Opțiuni de afișare';
 $string['page-mod-ardora-x'] = 'Orice pagină a modulului de fișier';
+$string['passinggrade'] = 'Aprobat';
+$string['passinggrade_help'] = 'Nota minimă pe care un utilizator trebuie să o obțină pentru a considera activitatea ca fiind promovată.';
 $string['pluginadministration'] = 'Administrarea modulului de fișier';
 $string['pluginname'] = 'Ardora';
-$string['popupheight'] = 'Înălțimea ferestrei pop-up (în pixeli)';
-$string['popupheightexplain'] = 'Specifică înălțimea implicită a ferestrelor pop-up.';
-$string['popupwidth_desc'] = 'Lățimea implicită a ferestrei pop-up (în pixeli).';
-$string['popupheight_desc'] = 'Înălțimea implicită a ferestrei pop-up (în pixeli).';
 $string['popupardora'] = 'Acest ardora ar trebui să apară într-o fereastră pop-up.';
 $string['popupardoralink'] = 'Dacă nu apare, faceți clic aici: {$a}';
+$string['popupheight'] = 'Înălțimea ferestrei pop-up (în pixeli)';
+$string['popupheight_desc'] = 'Înălțimea implicită a ferestrei pop-up (în pixeli).';
+$string['popupheightexplain'] = 'Specifică înălțimea implicită a ferestrelor pop-up.';
 $string['popupwidth'] = 'Lățimea ferestrei pop-up (în pixeli)';
+$string['popupwidth_desc'] = 'Lățimea implicită a ferestrei pop-up (în pixeli).';
 $string['popupwidthexplain'] = 'Specifică lățimea implicită a ferestrelor pop-up.';
 $string['printintro'] = 'Afișați descrierea ardora';
 $string['printintroexplain'] = 'Afișați descrierea ardora sub conținut? Unele tipuri de afișare pot să nu afișeze descrierea chiar dacă este activată.';
 $string['privacy:metadata'] = 'Pluginul ardora de fișier nu stochează date personale.';
-$string['ardora:addinstance'] = 'Adăugați un nou ardora';
-$string['ardoracontent'] = 'Fișiere și subfoldere';
-$string['ardoradetails_sizetype'] = '{$a->size} {$a->type}';
-$string['ardoradetails_sizedate'] = '{$a->size} {$a->date}';
-$string['ardoradetails_typedate'] = '{$a->type} {$a->date}';
-$string['ardoradetails_sizetypedate'] = '{$a->size} {$a->type} {$a->date}';
-$string['ardora:exportardora'] = 'Exportați ardora';
-$string['ardora:view'] = 'Vizualizați ardora';
-$string['ardora:grade'] = 'Evaluează livrările Ardora';
-$string['passinggrade'] = 'Aprobat';
-$string['passinggrade_help'] = 'Nota minimă pe care un utilizator trebuie să o obțină pentru a considera activitatea ca fiind promovată.';
-$string['invalidpassinggrade'] = 'Nota de trecere trebuie să fie un număr între 0 și 100.';
-$string['completionpassgrade'] = 'Studentul trebuie să obțină nota de trecere pentru a finaliza această activitate.';
-$string['completionpassgrade_help'] = 'Dacă această opțiune este activată, activitatea va fi marcată ca finalizată doar atunci când studentul obține nota de trecere specificată.';
-$string['maximumgrade'] = 'Nota maximă';
-$string['maximumgrade_help'] = 'Specificați nota maximă care poate fi obținută pentru această activitate.';
-
-$string['gradingoptions'] = 'Opțiuni de notare';
 $string['search:activity'] = 'Ardora';
 $string['selectmainfile'] = 'Vă rugăm să selectați fișierul principal făcând clic pe pictograma de lângă numele fișierului.';
 $string['showdate'] = 'Afișați data încărcării/modificării';
@@ -119,10 +124,10 @@ $string['showdate_help'] = 'Afișează data încărcării/modificării lângă l
 
 Dacă există mai multe fișiere în acest ardora, se afișează data încărcării/modificării fișierului principal.';
 $string['showsize'] = 'Afișați dimensiunea';
+$string['showsize_desc'] = 'Afișați dimensiunea fișierului pe pagina cursului?';
 $string['showsize_help'] = 'Afișează dimensiunea fișierului, cum ar fi "3.1 MB", lângă linkurile către fișier.
 
 Dacă există mai multe fișiere în acest ardora, se afișează dimensiunea totală a tuturor fișierelor.';
-$string['showsize_desc'] = 'Afișați dimensiunea fișierului pe pagina cursului?';
 $string['showtype'] = 'Afișați tipul';
 $string['showtype_desc'] = 'Afișați tipul fișierului (de exemplu, "Document Word") pe pagina cursului?';
 $string['showtype_help'] = 'Afișează tipul fișierului, cum ar fi "Document Word", lângă linkurile către fișier.
@@ -131,8 +136,3 @@ Dacă există mai multe fișiere în acest ardora, se afișează tipul fișierul
 
 Dacă tipul fișierului nu este cunoscut de sistem, acesta nu va fi afișat.';
 $string['uploadeddate'] = 'Încărcat {$a}';
-$string['embedheightexplain'] = 'Înălțimea cadrului scenei.';
-$string['embedwidthexplain'] = 'Lățimea cadrului scenei.';
-$string['embedwidth'] = 'Lățime';
-$string['embedheight'] = 'Înălțime';
-$string['courseidnotfound'] = 'ID-ul cursului nu a fost găsit.';
